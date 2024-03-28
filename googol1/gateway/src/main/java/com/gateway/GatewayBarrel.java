67dc5d7c82c5071d8e7632564d6474438b0fdbb5
fx310ferreira <fx31003@gmail.com>
2024-03-28 01:05:05 +0000

    implemented more presistence

--- googol1/gateway/src/main/java/com/gateway/GatewayBarrel.java
+++ googol1/gateway/src/main/java/com/gateway/GatewayBarrel.java
@@ -10,38 +10,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GatewayBarrel extends UnicastRemoteObject implements GatewayBarrelInt {
-    ArrayList<String> barrel_ids;
-    Map<String, BarrelInt> barrels;
+  ArrayList<String> barrel_ids;
+  Map<String, BarrelInt> barrels;
 
-    GatewayBarrel() throws RemoteException {
-        super();
-        this.barrel_ids = new ArrayList<>();
-        this.barrels = new HashMap<>();
-    }
+  GatewayBarrel() throws RemoteException {
+    super();
+    this.barrel_ids = new ArrayList<>();
+    this.barrels = new HashMap<>();
+  }
 
-    private BarrelInt getBarrel() throws RuntimeException{
-        if(barrel_ids.isEmpty())
-            throw new RuntimeException("No barrels available");
-        return barrels.get(barrel_ids.get(0));
-    }
+  private BarrelInt getBarrel() throws RuntimeException {
+    if (barrel_ids.isEmpty())
+      throw new RuntimeException("No barrels available");
+    return barrels.get(barrel_ids.get(0));
+  }
 
-    @Override
-    public boolean indexedUrl(String message) {
-            try {
-                return getBarrel().indexedUrl(message);
-            } catch (RemoteException e) {
-                throw new RuntimeException(e);
-            }
+  @Override
+  public boolean indexedUrl(String message) {
+    try {
+      return getBarrel().indexedUrl(message);
+    } catch (RemoteException e) {
+      // disconeotu do barrel
+      throw new RuntimeException(e);
     }
+  }
 
-    @Override
-    public synchronized void subscribe(BarrelInt barrel, String barrelId) throws RemoteException {
-        if (!barrels.containsKey(barrelId)) {
-            barrels.put(barrelId, barrel);
-            barrel_ids.add(barrelId);
-            System.out.println("Barrel with ID " + barrelId + " subscribed.");
-        } else {
-            System.out.println("Barrel with ID " + barrelId + " is already subscribed.");
-        }
+  @Override
+  public synchronized void subscribe(BarrelInt barrel, String barrelId) throws RemoteException {
+    if (!barrels.containsKey(barrelId)) {
+      barrels.put(barrelId, barrel);
+      barrel_ids.add(barrelId);
+      System.out.println("Barrel with ID " + barrelId + " subscribed.");
+    } else {
+      System.out.println("Barrel with ID " + barrelId + " is already subscribed.");
     }
+  }
 }
