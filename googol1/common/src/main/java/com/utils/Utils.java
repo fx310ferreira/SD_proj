272282813b8e2b98ca7ecad3a47a24a2962423f5
fx310ferreira <fx31003@gmail.com>
2024-04-03 11:25:28 +0100

    Fixed minor bug

--- googol1/common/src/main/java/com/utils/Utils.java
+++ googol1/common/src/main/java/com/utils/Utils.java
@@ -12,28 +12,27 @@ public class Utils {
 
 
         public static  String readProperties(Object obj, String filed, String defaultVal, String propertiesFile) {
+            try (InputStream input = obj.getClass().getClassLoader().getResourceAsStream(propertiesFile)) {
+                if (input == null){
+                    System.out.println("Unable to find " + propertiesFile + " defaulting to: " + defaultVal);
+                    return defaultVal;
+                }
 
-        try (InputStream input = obj.getClass().getClassLoader().getResourceAsStream(propertiesFile)) {
-            if (input == null){
-                System.out.println("Unable to find " + propertiesFile + " defaulting to: " + defaultVal);
+                Properties prop = new Properties();
+                prop.load(input);
+
+                if (prop.getProperty(filed) != null) {
+                    System.out.println("Using address: " + prop.getProperty(filed));
+                    return prop.getProperty(filed);
+                }else {
+                    System.out.println("Unable to find property defaulting to: " + defaultVal);
+                    return defaultVal;
+                }
+
+            } catch (Exception e) {
+                System.err.println("Error reading " + propertiesFile + " defaulting to: " + defaultVal);
                 return defaultVal;
             }
-
-            Properties prop = new Properties();
-            prop.load(input);
-
-            if (prop.getProperty(filed) != null) {
-                System.out.println("Using address: " + prop.getProperty(filed));
-                return prop.getProperty(filed);
-            }else {
-                System.out.println("Unable to find property defaulting to: " + defaultVal);
-                return defaultVal;
-            }
-
-        } catch (Exception e) {
-            System.err.println("Error reading " + propertiesFile + " defaulting to: " + defaultVal);
-            return defaultVal;
-        }
     }
 
 }
